0104a5e5692b41d37a18c9dbd935f93cf9fa77a0
sadamdjon@gmail.com <sadamjondjabarov@gmail.com>
2023-08-09 10:24:37 +0300

    adaptive version visor-landing

--- script/script.js
+++ script/script.js
@@ -19,3 +19,43 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+const headerLink = document.querySelectorAll('.header__link');
+console.log(btnNavEl, headerEl);
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+headerLink.forEach((link) => {
+  link.addEventListener('click', function () {
+    headerEl.classList.toggle('nav-open');
+  });
+});
+
+const items = document.querySelectorAll('.preferences__items');
+const dot = document.querySelectorAll('.dot');
+
+let speed = 3000;
+let index = 0;
+
+function startInterval() {
+  setInterval(() => {
+    index++;
+    if (index === items.length) index = 0;
+    updateContent();
+  }, speed);
+}
+startInterval();
+
+function updateContent() {
+  items.forEach((item) => {
+    item.classList.remove('active');
+  });
+  items[index].classList.add('active');
+  dot.forEach((el) => {
+    el.classList.remove('dot--fill');
+  });
+  dot[index].classList.add('dot--fill');
+}
+updateContent();
